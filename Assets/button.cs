688731704d1e9a39ce8a0c3153c3d63e9bb47dc7
JoshuaCrisp <joshuascottcrisp@me.com>
2017-09-18 19:27:34 -0400

    First Game Upload
    I could not get Milo’s script to add. This is likely because I did not
    have an Arduino connected.

---
+++ Assets/button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+
+	public float current = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			current += 2f;
+			transform.Translate (0, current, 0);
+		}
+	}
+	
+}
+
